d89ce864ff73a4f028c4b2935ad73cc1ad274045
MANAV BHARDWAJ <manavbhardwaj0008@gmail.com>
2026-08-18 22:12:57 +0530

    Time: 8 ms (16.28%), Space: 20.7 MB (24.27%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        if(nums.size()==0){
+            return 0;
+        }
+        if(nums.size()==1){
+            return nums[0];
+        }
+        for(int i=0;i<nums.size();i++){
+            if(i==0 && nums[i]!=nums[i+1]){
+                return nums[i];
+            }
+            else if(i==nums.size()-1 && nums[i]!=nums[i-1]){
+                return nums[i];
+            }
+            else if(i!=0 && i!=nums.size()-1){
+                if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]){
+                    return nums[i];
+                }
+            }
+            
+        }
+        return -1;
+    }
+};
