962cdb4aee447cdc5eae38fcbbbdca379f408172
MANAV BHARDWAJ <manavbhardwaj0008@gmail.com>
2026-08-19 11:03:35 +0530

    Time: 0 ms (100%), Space: 11.5 MB (79.36%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i=0;
+        int l=0;
+        int r= nums.size()-1;
+        while(i<=r){
+            if(nums[i]==0){
+                swap(nums[i],nums[l]);
+                l++;
+            }
+            else if(nums[i]==2){
+                swap(nums[i],nums[r]);
+                r--;
+                continue;
+            }
+         
+                i++;
+            
+            
+        }
+
+    
+    }
+};
