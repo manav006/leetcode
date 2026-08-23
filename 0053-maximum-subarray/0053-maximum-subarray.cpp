e12fe598775226eca14f5a0733f10cfc6227a57d
MANAV BHARDWAJ <manavbhardwaj0008@gmail.com>
2026-08-23 11:12:56 +0530

    Time: 4 ms (10.56%), Space: 71.8 MB (14.96%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int maxsum=nums[0];
+        int sum=nums[0];
+        for(int i=1;i<nums.size();i++){
+            sum=max(sum+nums[i],nums[i]);
+            maxsum=max(maxsum,sum);
+        }
+        return maxsum;
+    }
+};
