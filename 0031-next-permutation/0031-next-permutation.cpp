af5a7c417289c525a7be81973dd6cf1cc2370a4e
MANAV BHARDWAJ <manavbhardwaj0008@gmail.com>
2026-08-23 21:41:18 +0530

    Time: 0 ms (100%), Space: 15.9 MB (15.12%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int idx =-1;
+        int idx=-1;
         for(int i=nums.size()-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
                 idx=i;
@@ -10,9 +10,8 @@ public:
         }
         if(idx==-1){
             reverse(nums.begin(),nums.end());
-            return;
+            return ;
         }
-
 
         for(int i=nums.size()-1;i>=0;i--){
             if(nums[i]>nums[idx]){
@@ -20,8 +19,6 @@ public:
                 break;
             }
         }
-
         reverse(nums.begin()+idx+1,nums.end());
-
     }
 };
