ee1a6cca6d73fb22c53d9a31d47350d8b580059d
MANAV BHARDWAJ <manavbhardwaj0008@gmail.com>
2026-08-26 23:55:35 +0530

    Time: 0 ms (100%), Space: 9.7 MB (29.33%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -3,12 +3,12 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>>ans;
         for(int i=0;i<numRows;i++){
-            vector<int>v;
-            for(int j=0;j<i+1;j++){
+            vector<int>v(i+1);
+            for(int j=0;j<v.size();j++){
                 if(j==0 || j==i){
-                    v.push_back(1);
+                    v[j]=1;
                 }else{
-                    v.push_back(ans[i-1][j-1]+ans[i-1][j]);
+                    v[j]=ans[i-1][j-1]+ans[i-1][j];
                 }
             }
             ans.push_back(v);
